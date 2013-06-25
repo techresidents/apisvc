14ba4185cb98fe8baaa0238feca0ee0f04cf8b97
bmullins <bmullins@techresidents.com>
2013-06-25 13:59:23 -0400

    Update the user_search resource to use the updated location data (the 'region' field).

--- apisvc/resources/user_search.py
+++ apisvc/resources/user_search.py
@@ -21,9 +21,7 @@ class Skill(Struct):
 class LocationPref(Struct):
     id = fields.IntegerField()
     location_id = fields.IntegerField()
-    name = fields.StringField(filter_ext=".raw")
-    city = fields.StringField()
-    state = fields.StringField()
+    region = fields.StringField(filter_ext=".raw")
 
 class PositionPref(Struct):
     id = fields.IntegerField()
@@ -48,7 +46,7 @@ class UserSearchResource(Resource):
             "yrs_experience": ["eq", "in", "range", "ranges"],
             "joined": ["eq", "in", "range", "ranges"],
             "skills__name": ["eq", "in"],
-            "location_prefs__name": ["eq", "in"],
+            "location_prefs__region": ["eq", "in"],
             "position_prefs__type": ["eq", "in"],
             "technology_prefs__name": ["eq", "in"]
         }
@@ -85,8 +83,8 @@ class UserSearchResource(Resource):
             es_field="skills.name.raw",
             size_option="f_skills_size")
     f_location_prefs = TermsFacet(title="Location Preferences",
-            field="location_prefs__name",
-            es_field="location_prefs.name.raw",
+            field="location_prefs__region",
+            es_field="location_prefs.region.raw",
             size_option="f_location_prefs_size")
     f_position_prefs = TermsFacet(title="Position Preferences",
             field="position_prefs__type",
